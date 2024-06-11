f2155a7f42775c0f78fd5b51f778fd2f7fbb2e51
Erfan Jamali <jamaliseyederfan@gmail.com>
2024-06-11 19:14:22 +0330

    date compatibility added

---
+++ my-worker/.wrangler/tmp/deploy-6KXYXQ/worker.js
@@ -0,0 +1,10 @@
+// worker.js
+var worker_default = {
+  async fetch(request, env) {
+    return new Response("Hello wccccccccccccccccccccccccccccccccorld");
+  }
+};
+export {
+  worker_default as default
+};
+//# sourceMappingURL=worker.js.map
